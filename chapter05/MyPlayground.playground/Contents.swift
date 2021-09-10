a479dad273b861bfe43e6fa0a76eab762c16b202
tensaitensai <4619055.tus@gmail.com>
2021-09-10 21:07:55 +0900

    chapter06(p162)まで終了

---
+++ chapter05/MyPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var slider = UISlider()
+//実習5-3(p139)
+slider.setValue(1.0, animated: false)
+
---
+++ chapter05/実習5-4.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+class Taiyaki {
+    var nakami = "あんこ"
+    func sayNakami(){
+        print("中身は" + nakami + "です。")
+    }
+}
+
+var taiyaki = Taiyaki()
+taiyaki.nakami = "クリーム"
+taiyaki.sayNakami()
